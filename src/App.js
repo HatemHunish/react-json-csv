d81b562969d9e5d5caa93255a4fb16ecc5f63dae
Hatem Alselwi <hatem.alselwi@adecco.com>
2024-06-07 21:37:11 +0800

    fix build issu e

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
           <AppBar position="static">
             <Toolbar>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                PROD Query Runner
+                JSON To CSV Converter
               </Typography>
             </Toolbar>
           </AppBar>
